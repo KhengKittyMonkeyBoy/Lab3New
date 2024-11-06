9c5f200f8fbee72231686ef969c1b41133290283
Alfie <alfie.23@ichat.sp.edu.sg>
2024-11-06 11:24:39 +0800

    Lab3 things added

---
+++ Lab3_new/BMI.py
@@ -0,0 +1,31 @@
+def calculate_bmi(height, weight):
+    height = float(height)
+    weight = float(weight)
+    print("Height =", height)
+    print("Weight =", weight)
+    
+    BMI = weight / (height ** 2)
+    print("Your BMI is =", round(BMI,2))
+    print ("Weight Classification:" ,end="")
+
+
+    if BMI <18.5:
+        print("Underweight")
+        return -1
+    
+    elif BMI >25.0:
+        print ("Overweight")
+        return 1
+    else:
+        print("Normal")
+        return 0
+
+
+result = calculate_bmi(weight="57", height="1.73")
+result = calculate_bmi(weight="457", height="1.4")
+result = calculate_bmi(weight="57", height="1.90")
+
+print("Result=",result)
+
+
+
